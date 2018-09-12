afc8f73d789c1d728826f4674de5290e9e218368
march-dave <ysangy@gmail.com>
2018-09-11 21:52:34 -0400

    UI changing

--- src/js/App.js
+++ src/js/App.js
@@ -184,7 +184,7 @@ class App extends React.Component {
             </div>
           </div>
           <div className="col-sm">
-            Login
+            <div style={{ paddingTop: "7px"}}>Login</div>
           </div>
         </div>
 
